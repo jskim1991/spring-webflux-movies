4530e9b92a70ac55e8ca2e210c5ad2c7c3d405ca
kingj <tensaijskim1991@gmail.com>
2022-01-09 21:14:11 +0900

    first commit

---
+++ movies-info-service/src/main/java/io/jay/moviesinfoservice/controller/FluxAndMonoController.java
@@ -0,0 +1,31 @@
+package io.jay.moviesinfoservice.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> flux() {
+        return Flux.just(1,2,3)
+                .log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> mono() {
+        return Mono.just("hello world")
+                .log();
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        return Flux.interval(Duration.ofMillis(500))
+                .log();
+    }
+}
---
+++ movies-info-service/src/test/java/io/jay/moviesinfoservice/MoviesInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jay.moviesinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
